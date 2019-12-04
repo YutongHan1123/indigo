2fcae5107c9559f43d9b2c81efb449cd6b6ced95
YutongHan1123 <42613354+YutongHan1123@users.noreply.github.com>
2019-12-03 21:36:56 -0500

    Add files via upload

---
+++ js/gif.js
@@ -0,0 +1,39 @@
+function gif00()
+{
+  document.getElementById("myform").src="gif/success.gif";
+}
+
+
+// button
+function gif0()
+{
+  document.getElementById("picture").src="gif/success.gif";
+}
+
+function gif()
+{
+  document.getElementById("picture").src="gif/test2.gif";
+}
+function gif1()
+{
+  document.getElementById("picture").src="gif/test2.gif";
+}
+function gif2()
+{
+  document.getElementById("picture").src="gif/test.gif";
+}
+
+function gif3()
+{
+  document.getElementById("picture").src="gif/test3.gif";
+}
+
+function gif4()
+{
+  document.getElementById("picture").src="https://media.giphy.com/media/ASd0Ukj0y3qMM/giphy.gif";
+}
+
+function gif5()
+{
+  document.getElementById("picture").src="gif/whole.jpg";
+}
