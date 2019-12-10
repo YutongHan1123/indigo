d1df88accb4420245f7355c3d7a59c8bb9f27f0e
YutongHan1123 <42613354+YutongHan1123@users.noreply.github.com>
2019-12-10 09:00:25 -0500

    Add files via upload

--- js/gif.js
+++ js/gif.js
@@ -1,39 +1,23 @@
+var imageSources = ["gif/success.gif", "gif/success2.gif", "gif/success3.gif", "gif/success4.gif"]
+var buttons = document.getElementsByClassName("button")[0];
+var index = 0;
+
+      // if(buttons){
+      //   buttons.addEventListener('click', function() {
+      //     if (index === imageSources.length) {
+      //       index = 0;
+      //     }
+      //     document.getElementById("myform").src = imageSources[index];
+      //     index++;
+      //   });
+      // }
+
 function gif00()
 {
-  document.getElementById("myform").src="gif/success.gif";
-}
-
-
-// button
-function gif0()
-{
-  document.getElementById("picture").src="gif/success.gif";
-}
-
-function gif()
-{
-  document.getElementById("picture").src="gif/test2.gif";
-}
-function gif1()
-{
-  document.getElementById("picture").src="gif/test2.gif";
-}
-function gif2()
-{
-  document.getElementById("picture").src="gif/test.gif";
-}
-
-function gif3()
-{
-  document.getElementById("picture").src="gif/test3.gif";
-}
-
-function gif4()
-{
-  document.getElementById("picture").src="https://media.giphy.com/media/ASd0Ukj0y3qMM/giphy.gif";
-}
-
-function gif5()
-{
-  document.getElementById("picture").src="gif/whole.jpg";
+    if (index === imageSources.length) {
+      index = 0;
+    }
+    document.getElementById("myform").src = imageSources[index];
+    index++;
+  // document.getElementById("myform").src="gif/success.gif";
 }
